bada6383d26d32163b1f80e842c029d355e38eb6
Unity-2D-Cube <49322919+Unity-2D-Cube@users.noreply.github.com>
2024-05-05 17:09:45 +0200

    #Added backend + frontend weather funcionality (search the city to get weather info) #TO DO Fix broken weather icons showing after search response

---
+++ WeatherWebApp/Controllers/WeatherController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using WeatherWebApp.Models;
+
+namespace WeatherWebApp.Controllers
+{
+    public class WeatherController : Controller
+    {
+        private const string ApiKey = "3d54c044895ee1e27c19d8b0bc69f2fb";
+        private const string ApiBaseUrl = "https://api.openweathermap.org/data/2.5/weather";
+        private const string IconBaseUrl = "https://openweathermap.org/img/wn/";
+
+        public async Task<ActionResult> Index(string city)
+        {
+            if (string.IsNullOrEmpty(city))
+                return View();
+
+            var weatherData = await GetWeatherDataAsync(city);
+
+            return View(weatherData);
+        }
+
+        private async Task<WeatherData> GetWeatherDataAsync(string city)
+        {
+            using (var client = new HttpClient())
+            {
+                string apiUrl = $"{ApiBaseUrl}?q={city}&appid={ApiKey}&units=metric";
+                var response = await client.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    var weatherData = JsonConvert.DeserializeObject<WeatherData>(content);
+                    return weatherData;
+                }
+                else
+                {
+                    throw new Exception($"Failed to get weather data. Status code: {response.StatusCode}");
+                }
+            }
+        }
+
+        public string GetIconUrl(string iconId)
+        {
+            return $"{IconBaseUrl}{iconId}.png";
+        }
+    }
+}
---
+++ WeatherWebApp/Models/WeatherData.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+
+namespace WeatherWebApp.Models
+    
+{
+    public class WeatherData
+    {
+        [JsonProperty("main")]
+        public Main Main { get; set; }
+
+        [JsonProperty("weather")]
+        public List<Weather> WeatherList { get; set; }
+
+        [JsonProperty("wind")]
+        public Wind Wind { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+
+    public class Main
+    {
+        [JsonProperty("temp")]
+        public double Temperature { get; set; }
+
+        [JsonProperty("humidity")]
+        public int Humidity { get; set; }
+    }
+    public class Weather
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("icon")]
+        public string Icon { get; set; }
+        
+    }
+
+    public class Wind
+    {
+        [JsonProperty("speed")]
+        public double Speed { get; set; }
+    }
+}
